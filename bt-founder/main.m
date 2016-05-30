4e5425475be8f66c0899d94ecbf033cefaac01fb
Adison Wu <adison.wu@gmail.com>
2016-05-30 17:34:56 +0800

    Initial Commit

---
+++ bt-founder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bt-founder
+//
+//  Created by Adison Wu on 2016/5/30.
+//  Copyright © 2016年 adison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
